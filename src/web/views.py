0efd823dd0146a8d5867a49c524bf898c5dc5a55
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2022-11-10 15:23:38 +0800

    eer

--- src/web/views.py
+++ src/web/views.py
@@ -122,7 +122,7 @@ def award(request):
     context = {
         'com':company,
         'timeline':timeline,
-        'about':'Award and Achievement',
+        'about':'Award and Our Accomplishment',
         'post':post
     }
 
