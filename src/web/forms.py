e0410bf634e804e35ad162242f295d9feeb4967a
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2022-12-13 02:36:06 +0800

    Update forms.py

--- src/web/forms.py
+++ src/web/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.db import models
 from .models import Contact
 from django_countries import Countries
+from django.db.models.fields import BLANK_CHOICE_DASH
 
 class ContactForm(forms.ModelForm):
 
@@ -15,11 +16,13 @@ class ContactForm(forms.ModelForm):
         ('Others','Others'),
     )
 
+    CON = Countries
+
     contactname = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Name Here'}))
     contactemail = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Email Here'}))
     companyname = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Please insert your company name'}))
     looking = forms.ChoiceField(choices=Find, required=False, widget=forms.Select(attrs={'class':'form-control'}))
-    country = forms.ChoiceField(choices=Countries, required=False, widget=forms.Select(attrs={'class':'form-control'}))
+    country = forms.ChoiceField(choices=CON, required=False, widget=forms.Select(attrs={'class':'form-control'}))
     contacttel = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     enquirysubject = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your subject enquiry here'}))
