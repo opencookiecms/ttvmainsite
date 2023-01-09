954bc75a159b2022106a5312f002a0a9d4509a31
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-01-09 15:27:51 +0800

    upload

--- src/static/js/lostandfound.js
+++ src/static/js/lostandfound.js
@@ -15,3 +15,49 @@ $(document).ready(function () {
    }
 
 });
+
+
+$(document).ready(function () {
+   var links = document.querySelectorAll('.lop a')
+   for (var i = 0; i < links.length; i++){
+     var href =links[i].href   
+     var httpss = links[i].href
+     if(href){
+        links[i].href=href.replace('http://','https://');
+        links[i].innerHTML = "More Info"
+        links[i].className= "btn btn-primary"
+      
+       
+     }
+     if(httpss){
+        links[i].href=href.replace(/localhost:8000/, encodeURIComponent("www.bursamalaysia.com"));
+     }
+     
+   }
+
+});
+
+
+
+$(document).ready(function () {
+   var links = document.querySelectorAll('.tags a')
+   
+ 
+
+   for (var i = 0; i < links.length; i++){
+     var href =links[i].href 
+     var httpss = links[i].href
+     if(href){
+   
+        links[i].href=href.replace('http://','https://');
+       
+     }
+     if(httpss){
+        links[i].href=href.replace(/localhost:8000/, encodeURIComponent("www.bursamalaysia.com"));
+     }
+     
+   }
+
+});
+
+
