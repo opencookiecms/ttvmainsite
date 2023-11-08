26f294c22649539c50dbd0daede77b6eabe57843
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-08 09:23:25 +0800

    added individual pages' keywords

--- src/web/views.py
+++ src/web/views.py
@@ -67,7 +67,7 @@ def vision(request):
         'pv':postvision,
         'hero5':hero5,
         'hero7':hero7,
-        
+        'keywords':"innovate, equipment inspection, innovate equipment inspection, leading brand, equipment vision inspection, vision inspection"
     }
 
     
