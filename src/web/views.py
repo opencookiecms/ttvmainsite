0723885618dae12c4776a38206229a41890d83a3
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-22 11:45:07 +0800

    .

--- src/web/views.py
+++ src/web/views.py
@@ -353,7 +353,6 @@ def icled(request, slug):
 #robotic mainpage
 def robotic(request):
     pr = Product.objects.filter(productcategory = 'Robotic')
-    print("No.",pr.count())
     company = Company.objects.get(id=1)
     context = {
         'pr': pr,
