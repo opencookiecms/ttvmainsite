921b4d8782ffdba9b213c6641697e41199f82c44
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-23 17:43:16 +0800

    Update models.py

--- src/web/models.py
+++ src/web/models.py
@@ -449,4 +449,4 @@ class Hr(models.Model):
     internship = models.CharField(max_length=50, null=True, blank=True)
     country = models.CharField(max_length=50, null=False, blank=False)
     contacttel = models.CharField(max_length=20, null=False, blank=False)
-    resume = models.FileField(upload_to='resumes/')
+    resume = models.FileField()
