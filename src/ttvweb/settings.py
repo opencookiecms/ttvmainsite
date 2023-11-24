648b6deaed1440ba228d9300e6582c555184e1a7
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-24 15:53:19 +0800

    test

--- src/ttvweb/settings.py
+++ src/ttvweb/settings.py
@@ -153,7 +153,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ttvision.net@ttvision-tech.com'
+EMAIL_HOST_USER = 'ttvision_net@ttvision-tech.com'
 EMAIL_HOST_PASSWORD = 'TTVision2023'
 SERVER_EMAIL = EMAIL_HOST_USER
 
