8e403dd123d396a44a3080253d71d94544afa989
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-27 16:56:47 +0800

    Update admin.py

--- src/web/admin.py
+++ src/web/admin.py
@@ -6,19 +6,14 @@ from web.models import Accordation, Annoucement, Category, Company, Contact, Eve
 #configs
 class ProductfeasAdmin(admin.ModelAdmin):
 #make the data in the category field persistent after clicking add another
-    # def response_add(self, request, obj, post_url_continue=None):
-    #     print("response add is called")
-    #     if "_addanother" in request.POST:
-    #         request.POST = request.POST.copy()
-    #         request.POST['product'] = str(obj.product)
-    #         return super().response_add(request, obj, post_url_continue)
-    #     else:
-    #         return super().response_add(request, obj)
-
-    def save_model(self, request, obj, form, change):
+    def response_add(self, request, obj, post_url_continue=None):
+        print("response add is called")
         if "_addanother" in request.POST:
-            form.cleaned_data['product'] = obj.product.id
-        super().save_model(request, obj, form, change)
+            request.POST = request.POST.copy()
+            request.POST['product'] = str(obj.product.id)
+            return super().response_add(request, obj, post_url_continue)
+        else:
+            return super().response_add(request, obj)
 
 
 # Register your models here.
