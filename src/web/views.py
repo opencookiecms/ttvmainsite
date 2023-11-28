75f93a5ba7093b0502710897fe638dc1de3806b9
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2023-11-28 10:35:05 +0800

    Update views.py

--- src/web/views.py
+++ src/web/views.py
@@ -9,7 +9,6 @@ from .forms import ContactForm, NewsletterForm, HrForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import json
 import requests
-import mimetypes
 
 # Create your views here.
 
@@ -636,8 +635,8 @@ def hrForm(request):
             #email contents
             subject = 'Job Application for ' + job 
             message = f'Name: {form.cleaned_data["contactname"]}\nEmail: {form.cleaned_data["contactemail"]}\nPhone Number: {form.cleaned_data["contacttel"]}\nCountry: {form.cleaned_data["country"]}\nJob Position Applied: {job} ({form.cleaned_data["jobtype"]})'
-            resume = form.cleaned_data["resume"]
-            appform = form.cleaned_data["appform"]
+            resume = form["resume"].file
+            appform = form["appform"].file
             #domain email
             from_email = settings.SERVER_EMAIL
             #recipient email
@@ -646,10 +645,8 @@ def hrForm(request):
             #attaching contents to the email to be sent
             email = EmailMessage(subject, message, from_email, recipient_list)
             #attaching files to email
-            mime_type_resume, _ = mimetypes.guess_type(resume.name)
-            mime_type_appform, _ = mimetypes.guess_type(appform.name)
-            email.attach(resume.name, resume.read(), mime_type_resume)
-            email.attach(appform.name, appform.read(), mime_type_appform)
+            email.attach(resume.name, resume.read(), resume.content_type)
+            email.attach(appform.name, appform.read(), appform.content_type)
             email.send()
             #redirect to success
             return redirect('contactdone')
