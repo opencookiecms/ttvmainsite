c8d6401be0826082c38ae45772f631109d8ed36c
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2024-01-22 10:11:59 +0800

    Update models.py

--- src/web/models.py
+++ src/web/models.py
@@ -155,9 +155,9 @@ class EventNews(models.Model):
         return self.ttitle
     
 class EventPhoto(models.Model):
-    event = models.ForeignKey(EventNews, blank=True, null=True,on_delete = models.SET_NULL)
-    img = models.ImageField(null=True, blank=True)
-    imgtitle = models.CharField(max_length=150, null=True,blank=True)
+    # event = models.ForeignKey(EventNews, blank=True, null=True,on_delete = models.SET_NULL)
+    # img = models.ImageField(null=True, blank=True)
+    # imgtitle = models.CharField(max_length=150, null=True,blank=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
