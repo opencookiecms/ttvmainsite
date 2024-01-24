702dae22e46510ab30d555795bcdcb34a2638a4b
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2024-01-24 11:01:06 +0800

    Update models.py

--- src/web/models.py
+++ src/web/models.py
@@ -551,8 +551,8 @@ class Job(models.Model):
 class Media(models.Model):
     medtitle = models.CharField(max_length=150, null=True,blank=True)
     vid = models.FileField(null=True, blank=True)
-    publish_date = models.DateTimeField()
-    published = models.BooleanField()
+    publish_date = models.DateTimeField(null=True)
+    published = models.BooleanField(default=False)
 
 #Request Form
 class ReqForm(models.Model):
