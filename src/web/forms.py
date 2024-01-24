68f444638cfef5333b399ca95aa356cc5b07b828
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2024-01-24 15:52:13 +0800

    Update forms.py

--- src/web/forms.py
+++ src/web/forms.py
@@ -615,7 +615,7 @@ class ReqForm(forms.ModelForm):
     capcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
     class Meta:
-        models=Req
+        model =Req
         fields = [
             'contactname', 
             'contactemail', 
