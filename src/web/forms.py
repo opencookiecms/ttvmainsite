db824cfd917bdabf32a7af52501e7d9442e2d34e
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2024-01-24 14:26:27 +0800

    Update forms.py

--- src/web/forms.py
+++ src/web/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db import models
-from .models import Contact, Newsletter, Hr
+from .models import Contact, Newsletter, Hr, ReqForm
 from django_countries import Countries
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV2Checkbox
@@ -615,6 +615,7 @@ class ReqForm(forms.ModelForm):
     capcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
     class Meta:
+        models=ReqForm
         fields = [
             'contactname', 
             'contactemail', 
